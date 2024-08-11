ebf5874b70a82d699b3b2d857d3cd4debe0d4eee
Dima_Orobchenko <dimaor1212@gmail.com>
2024-08-11 19:30:47 +0300

    add sort_by_date func in processing.py

--- scr/processing.py
+++ scr/processing.py
@@ -1,3 +1,9 @@
-def filter_by_state(id_list: list, state = 'EXECUTED') -> list | None:
+def filter_by_state(id_list: list, state: str = "EXECUTED") -> list | None:
     """Функция возвращает список словарей с соответствующим значением состояния"""
-    return [i for i in id_list if i['state'] == state]
+    return [i for i in id_list if i["state"] == state]
+
+
+def sort_by_date(id_list: list, ascending: bool = True) -> list | None:
+    """Функция сортирует список по дате"""
+    sorted_list = sorted(id_list, key=lambda id_operation: id_operation.get("date", 0), reverse=ascending)
+    return sorted_list
