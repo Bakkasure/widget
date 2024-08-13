a537f7362e94383049dc7c8575af2d8fff30b295
Dima_Orobchenko <dimaor1212@gmail.com>
2024-08-13 18:30:57 +0300

    fixes processing.py, test_masks.py, add test functions in test_processing.py

--- scr/processing.py
+++ scr/processing.py
@@ -1,9 +1,15 @@
 def filter_by_state(id_list: list, state: str = "EXECUTED") -> list | None:
     """Функция возвращает список словарей с соответствующим значением состояния"""
-    return [i for i in id_list if i["state"] == state]
+    if len(id_list) > 0:
+        return [i for i in id_list if i["state"] == state]
+    else:
+        return None
 
 
 def sort_by_date(id_list: list, ascending: bool = True) -> list | None:
     """Функция сортирует список по дате"""
-    sorted_list = sorted(id_list, key=lambda id_operation: id_operation.get("date", 0), reverse=ascending)
-    return sorted_list
+    if len(id_list) > 0:
+        sorted_list = sorted(id_list, key=lambda id_operation: id_operation.get("date", 0), reverse=ascending)
+        return sorted_list
+    else:
+        return None
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -4,10 +4,10 @@ from scr.masks import get_mask_account, get_mask_card_number
 
 
 @pytest.mark.parametrize("number, result", [("124587", None), ("", None), ("7001555521219784", "7001 55** **** 9784")])
-def test_masks_card(number, result):
+def test_masks_card(number: str, result: str | None) -> None:
     assert get_mask_card_number(number) == result
 
 
 @pytest.mark.parametrize("number, result", [("124587", None), ("", None), ("70015555212197841414", "**1414")])
-def test_masks_account(number, result):
+def test_masks_account(number: str, result: str | None) -> None:
     assert get_mask_account(number) == result
