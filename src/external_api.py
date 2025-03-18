39aefcd8341f43d9959c5baf768ab8b8e64eecfd
Dima_Orobchenko <dimaor1212@gmail.com>
2025-03-18 23:27:06 +0300

    Add tests for external_api.py and some fixes

--- src/external_api.py
+++ src/external_api.py
@@ -11,24 +11,18 @@ token = os.getenv("API_KEY")
 def convertion_to_rub(operation_info: dict) -> float | Any | None:
     """Функция конвертирует валюту и возвращает сумму в рублях"""
     try:
-        try:
-            if operation_info["operationAmount"]["currency"]["name"] != "руб.":
-                value_from = operation_info["operationAmount"]["currency"]["name"]
-                amount = operation_info["operationAmount"]["amount"]
+        if operation_info["operationAmount"]["currency"]["name"] != "руб.":
+            value_from = operation_info["operationAmount"]["currency"]["name"]
+            amount = operation_info["operationAmount"]["amount"]
+            param = {"from": value_from, "to": "RUB", "amount": amount}
+            header = {"apikey": token}
+            url = "https://api.apilayer.com/exchangerates_data/convert"
 
-                header = {
-                    "apikey": token,
-                }
-                url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={value_from}&amount={amount}"
-
-                response = requests.get(url, headers=header)
-                amount_rub = response.json()
-                return amount_rub["result"]
-            else:
-                return operation_info["operationAmount"]["amount"]
-        except KeyError:
-            print("Указанный ключ не найден")
-            return None
-    except TypeError:
-        print("Аргумент функции должен быть словарём")
+            response = requests.get(url, headers=header, params=param)
+            amount_rub = response.json()
+            return amount_rub.get("result")
+        else:
+            return operation_info["operationAmount"]["amount"]
+    except KeyError:
+        print("Указанный ключ не найден")
         return None
