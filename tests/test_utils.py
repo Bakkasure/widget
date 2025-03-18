f786be4d456c0a296cdd9a4fb175f1c7c67a700f
Dima_Orobchenko <dimaor1212@gmail.com>
2025-03-18 22:21:33 +0300

    Add tests for utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,24 +1,26 @@
-import json
-from unittest.mock import patch, mock_open
+from unittest.mock import mock_open, patch
 
 from src.utils import get_operations_list
 
 
-@patch("builtins.open", new_callable=mock_open, read_data='[{"key": "value"}]')
-@patch("json.load", return_value=[{"key": "value"}])
-def test_get_operations_valid_file(mock_json_file_load, mock_file_open) -> None:
-    assert get_operations_list('test_file.json')
-    mock_json_file_load.assert_called_once()
+def test_file_not_found() -> None:
+    """Функция возвращает пустой список при отсутствии файла"""
+    with patch("builtins.open", side_effect=FileNotFoundError):
+        result = get_operations_list("test.json")
+        assert result == []
 
 
-@patch('builtins.open', side_effect=FileNotFoundError)
-def test_get_operations_file_nit_found(mock_file_open) -> None:
-    assert get_operations_list('test_file.json') == []
-    mock_file_open.assert_called_once()
+def test_decode_error() -> None:
+    """Функция возвращает пустой список при ошибке чтения файла"""
+    mock_data = "[{'key':'value'},"
+    with patch("builtins.open", mock_open(read_data=mock_data)):
+        result = get_operations_list("test.json")
+        assert result == []
 
 
-@patch('builtins.open', new_callable=mock_open, read_data='что-то на русском')
-@patch('json.load', side_effect=json.JSONDecodeError('Ошибка декодирования файла', 'Неверный формат данных', 0))
-def test_get_operation_invalid_data_in_file(mock_json_file_load, mock_file_open) -> None:
-    assert get_operations_list('test_file.json') == []
-    mock_json_file_load.assert_called_once()
+def test_get_operations_list_success() -> None:
+    """Функция проверяет корректность возвращаемых данных"""
+    mock_data = '[{"key":"value"}]'
+    with patch("builtins.open", mock_open(read_data=mock_data)):
+        result = get_operations_list("test.json")
+        assert result == [{"key": "value"}]
